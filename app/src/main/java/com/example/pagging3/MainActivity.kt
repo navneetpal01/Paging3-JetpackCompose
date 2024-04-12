b9ff938ad9681c17dab290ab94517bb2ce88255e
navneet <navneetpalgithub@gmail.com>
2024-04-12 17:15:49 +0530

    12

---
+++ app/src/main/java/com/example/pagging3/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pagging3
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pagging3/Paging3Application.kt
@@ -0,0 +1,11 @@
+package com.example.pagging3
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+
+@HiltAndroidApp
+class Paging3Application : Application(){
+
+}
